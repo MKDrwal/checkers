20480e6032d2f6f0180e25f526bb14527b5e07d4
Mateusz Kowalczyk <mateusz.kowalczyk24@gmail.com>
2019-04-24 10:09:04 +0200

    Created proposal where pawn can move

--- Zadanie2/main.js
+++ Zadanie2/main.js
@@ -1,6 +1,33 @@
 $(function() {
     $('#checkersBoard .field').sortable({
-        connectWith: ".field",
+        connectWith: ".proposal",
     });
     $('#checkersBoard .field').disableSelection();
+
+    $('.pawn').mousedown(function () {
+        let pawn = $(this);
+        let pawnRow = pawn.closest('.row');
+        let pawnRowId = pawnRow.index();
+        let allRows = $('#checkersBoard .row');
+
+        if(pawnRowId > 0 ) {
+            let row = $(allRows[pawnRowId - 1]);
+
+            let fields = row.find('.field');
+            $(fields[pawn.parent().index() + 1]).addClass('proposal').append('<div class="pawn shadow"></div>');
+            $(fields[pawn.parent().index() - 1]).addClass('proposal').append('<div class="pawn shadow"></div>');
+        }
+
+        if(pawnRowId < 7){
+            let row = $(allRows[pawnRowId + 1]);
+
+            let fields = row.find('.field');
+            $(fields[pawn.parent().index() + 1]).addClass('proposal').append('<div class="pawn shadow"></div>');
+            $(fields[pawn.parent().index() - 1]).addClass('proposal').append('<div class="pawn shadow"></div>');
+        }
+    });
+
+    $(document).mouseup(function(){
+        $('.proposal').removeClass('proposal').find('.shadow').remove();
+    });
 });
