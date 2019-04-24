b648fbe587eaf71e8c6fc0b9f6238194bdce20af
Mateusz Kowalczyk <mateusz.kowalczyk24@gmail.com>
2019-04-24 12:23:51 +0200

    Little cleanup code

--- Zadanie2/main.js
+++ Zadanie2/main.js
@@ -1,9 +1,6 @@
 $(function() {
-
-    let allFields = $('#checkersBoard .field');
     let allRows = $('#checkersBoard .row');
     let fields;
-    console.log(allFields);
 
     for(i=0;i<3;i++){
         z = (i%2) ? 1 : 0;
@@ -28,31 +25,18 @@ $(function() {
 
     $('.pawn').mousedown(function () {
         let pawn = $(this);
-        let pawnRow = pawn.closest('.row');
-        let pawnRowId = pawnRow.index();
-        //let allRows = $('#checkersBoard .row');
+        let pawnRowId = pawn.closest('.row').index();
 
-        if(pawnRowId > 0 ) {
-            let row = $(allRows[pawnRowId - 1]);
-
-            let fields = row.find('.field');
-
-            let right = $(fields[pawn.parent().index() + 1]);
-            if(right.children().length == 0){
-                right.addClass('proposal').append('<div class="pawn shadow"></div>');
-            }
-            let left = $(fields[pawn.parent().index() - 1]);
-            if(left.children().length == 0){
-                left.addClass('proposal').append('<div class="pawn shadow"></div>');
-            }
-            
+        if(pawn.hasClass('first')){
+            direction = 1;
+        } else {
+            direction = 0;
         }
 
-        if(pawnRowId < 7){
-            let row = $(allRows[pawnRowId + 1]);
-
+        if((direction == 1 && pawnRowId < 7) || (direction == 0 && pawnRowId > 0)){
+            let row = $(allRows[(direction == 1) ? pawnRowId + 1 : pawnRowId - 1]);
             let fields = row.find('.field');
-           
+
             let right = $(fields[pawn.parent().index() + 1]);
             if(right.children().length == 0){
                 right.addClass('proposal').append('<div class="pawn shadow"></div>');
