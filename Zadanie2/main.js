3eb08e66d57e97d35c90bb088194a95f59924d16
Mateusz Kowalczyk <mateusz.kowalczyk24@gmail.com>
2019-04-27 22:07:57 +0200

    Added capture the pawn

--- Zadanie2/main.js
+++ Zadanie2/main.js
@@ -37,18 +37,39 @@ $(function() {
             let row = $(allRows[(direction == 1) ? pawnRowId + 1 : pawnRowId - 1]);
             let fields = row.find('.field');
 
-            let right = $(fields[pawn.parent().index() + 1]);
-            if(right.children().length == 0){
-                right.addClass('proposal').append('<div class="pawn shadow"></div>');
-            }
-            let left = $(fields[pawn.parent().index() - 1]);
-            if(left.children().length == 0){
-                left.addClass('proposal').append('<div class="pawn shadow"></div>');
-            }
+            [1, -1].forEach(function(elem) {
+
+                field = $(fields[pawn.parent().index() + elem]);
+                if(field.children().length == 0){
+                    field.addClass('proposal').append('<div class="pawn shadow"></div>');
+                } else {
+                    occupated = $(field.children()[0]);
+                    if((pawn.hasClass('first') && occupated.hasClass('second')) || (pawn.hasClass('second') && occupated.hasClass('first'))) {
+                        
+                        fieldsO = $(allRows[(direction == 1) ? pawnRowId + 2 : pawnRowId - 2]).find('.field');
+                        fieldShot = $(fieldsO[pawn.parent().index() + ((elem == 1) ? 2 : -2)]);
+                        
+                        if(fieldShot.children().length == 0){
+                            fieldShot.addClass('proposal shot').append('<div class="pawn shadow "></div>');
+                            field.addClass('target');
+                        }
+                    }
+                }
+            });
         }
     });
 
     $(document).mouseup(function(){
         $('.proposal').removeClass('proposal').find('.shadow').remove();
+        let shot = $('.shot');
+        if(shot.length > 0){
+            
+            let target = $('.target');
+            if(shot.children().length > 0){
+                $(target.children()[0]).remove();
+            }
+            shot.removeClass('shot');
+            target.removeClass('target');
+        }
     });
 });
