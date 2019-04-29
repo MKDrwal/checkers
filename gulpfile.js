973216bf6c8e531968a4df67e0d40a630b45ad34
Mateusz Kowalczyk <mateusz.kowalczyk24@gmail.com>
2019-04-29 10:04:18 +0200

    Added gulp on project

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', (done) => {
+    return gulp.src('src/Sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError()))
+        .pipe(gulp.dest('./public/css/'));
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./src/Sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css/'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('src/Sass/**/*.scss', (done) => {
+        gulp.series(['sass'])(done);
+    })
+});
+
+gulp.task('default', function (done) {
+    gulp.series(['styles', 'watch'])(done);
+});
