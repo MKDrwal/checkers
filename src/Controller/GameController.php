2a4e58fea034d74f75762c509fd72b496eaec704
Mateusz Kowalczyk <mateusz.kowalczyk24@gmail.com>
2019-05-05 12:47:36 +0200

    Added set cookie with gameinfo

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -4,7 +4,9 @@
 namespace App\Controller;
 
 
+use App\Entity\GameInfo;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -16,9 +18,24 @@ class GameController extends AbstractController
      * @Route("/guest", name="gameGuestBoard")
      */
     public function showGuestBoard(){
+        $entityManager = $this->getDoctrine()->getManager();
+        $serializer = $this->container->get('serializer');
+
+        if(isset($_COOKIE['gameinfo'])) {
+            $game = $serializer->deserialize($_COOKIE['gameinfo'], GameInfo::class, 'json');
+        } else {
+            $game = new GameInfo();
+            $entityManager->persist($game);
+            $entityManager->flush();
+
+            setcookie('gameinfo', $serializer->serialize($game, 'json'), time() + 3600 * 24 * 30);
+        }
 
         return $this->render(
-            'Game/guest.html.twig'
+            'Game/guest.html.twig',
+            [
+                'game' => $game,
+            ]
         );
     }
 }
